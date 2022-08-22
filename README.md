8b3e210559f6f5e7f229fc0036badbf1095ed557
Alberto Robledo <roblo53@hotmail.com>
2022-08-22 13:57:04 -0300

    estructura y readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# VisitAr PWA
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) Angular CLI: 13.1.1
+Node: 16.13.1
+Package Manager: npm 8.1.2
+OS: linux x64
+
+<p align="center"><img src="https://github.com/BETOXL/VisitAR/blob/main/src/assets/images/"></p>
+
+## Install
+
+Run `npm install` for a dev server.
+
+## Development server
+
+Run `ionic serve` or `ng serve`  for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ionic build` or  `ng build` to build the project. The build artifacts will be stored in the `www/` or `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [BETOXL](https://BETOXL.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Deploy
+
+ng deploy --base-href=/VisitAR/
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  encuesta = [
+    { 
+      Grupo:"Ambiental" ,
+      EsMultipe: true,
+      Subgrupo : [
+        { 
+          Subgrupo: "Ubicacion",
+          pregutas:
+          [ 
+            {
+              id:1,
+              preguta:"Ubicacion",
+              opciones:["rural", "Urbna"]		
+            }
+          ],
+        },
+        { "Subgrupo": "Terreno",
+          "pregutas":
+          [ 
+              {
+                id:1,
+                preguta:"INSTITUCIONES BARRIALES / VECINALES",
+                tipo:"Combo",
+                opciones:[	
+                      "NO CONTESTA",
+                      "AUSENTE",
+                      "BALDÍO (SIN CERRAMIENTO)",
+                      "BALDÍO CON CERCA (Rejas, Alambre, Madera)",
+                      "BALDÍO CON TAPIA (Ladrillo, Bloque)",
+                      "VIVIENDA, ACEPTA LA ENCUESTA",
+                      "VIVIENDA, RECHAZO",
+                      "EQUIPAMIENTO COMERCIO",
+                      "SITUACIÓN DE CALLE",
+                      "VIVIENDA DESHABITADA",
+                      "VIVIENDA EN CONSTRUCCIÓN",
+                      "VIVIENDA TEMPORAL",
+                      "OTROS (Especificar)"
+                     ],
+                valor:"AUSENTE"
+              },
+              {
+                id:1.1,
+                condicion:"{{1}}=='OTROS ( Especificar )'",
+                preguta:"INSTITUCIONES BARRIALES / VECINALES DETALLE",
+                tipo:"Texto"
+              }
+          ]
+        }			
+            
+      ]
+    }
+  ];
   constructor() {}
 
 }
