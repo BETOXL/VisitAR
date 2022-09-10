2aaae003e1b8d7f830b6f5b21b54a079692f7137
Alberto Robledo <roblo53@hotmail.com>
2022-09-09 23:43:04 -0300

    se recorre el json y se crea los formularios se identifica los grupos multiples

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,36 +1,38 @@
 import { Component } from '@angular/core';
-
+import {FormGroup, Validators, FormControl, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
   encuesta = [
     { 
       Grupo:"Ambiental" ,
-      EsMultipe: true,
+      EsMultiple: false,
       Subgrupo : [
         { 
           Subgrupo: "Ubicacion",
           pregutas:
           [ 
             {
-              id:1,
+              id:2588,
               preguta:"Ubicacion",
-              opciones:["rural", "Urbna"]		
+              tipo:"Select",
+              opciones:["Rural", "Urbana"]		
             }
           ],
         },
-        { "Subgrupo": "Terreno",
-          "pregutas":
+        { Subgrupo: "Terreno",
+          pregutas:
           [ 
               {
-                id:1,
+                id:1258,
                 preguta:"INSTITUCIONES BARRIALES / VECINALES",
                 tipo:"Combo",
                 opciones:[	
-                      "NO CONTESTA",
+                      "NO CONTESTA1",
                       "AUSENTE",
                       "BALDÍO (SIN CERRAMIENTO)",
                       "BALDÍO CON CERCA (Rejas, Alambre, Madera)",
@@ -47,8 +49,8 @@ export class HomePage {
                 valor:"AUSENTE"
               },
               {
-                id:1.1,
-                condicion:"{{1}}=='OTROS ( Especificar )'",
+                id:1025,
+                condicion:"{{1258}}=='OTROS ( Especificar )'",
                 preguta:"INSTITUCIONES BARRIALES / VECINALES DETALLE",
                 tipo:"Texto"
               }
@@ -56,8 +58,77 @@ export class HomePage {
         }			
             
       ]
+    },
+    { 
+    Grupo:"Personas" ,
+    EsMultiple: true,
+    Subgrupo : [
+      { 
+        Subgrupo: "Datos Personales",
+        pregutas:
+        [
+          {
+            id:2581,
+            preguta:"Nom",
+            tipo:"Texto",
+          },
+          {
+            id:2582,
+            preguta:"Edad",
+            tipo:"Numero",
+          },
+          {
+            id:2583,
+            preguta:"Fecha Nacimiento",
+            tipo:"Fecha",
+          },
+          {
+            id:2184,
+            preguta:"Fumador?",
+            tipo:"Booleano",
+          }
+        ],
+      },
+    ]
     }
   ];
-  constructor() {}
+  formPreguntas: FormGroup;
+  inputsPreguntas = [];
+  GruposNombre=[];
+  SubGruposNombre=[];
+  SubGruposMultiples=[];
+  constructor(private formBuilder: FormBuilder) {
+    this.formPreguntas = formBuilder.group({});
+  }
+  async ionViewDidEnter(){
+    setTimeout(() => {
+      this.preparingInputsQuestions();
+    }, 500);
+  }
+  
+
+  preparingInputsQuestions(){
+    for (var grupos of this.encuesta){
+        //Veo los grupos existentes
+        console.log("Agrego un grupo ", grupos.Grupo);
+        this.GruposNombre.push(grupos.Grupo);
+        //Recorro los subgrupos
+        for (var subgrupos of grupos.Subgrupo){
+            console.log("Agrego un subgrupo ", subgrupos.Subgrupo);
+            this.SubGruposNombre.push(subgrupos.Subgrupo);
+            //Recorro las preguntas
+            if(grupos.EsMultiple==true){
+              this.SubGruposMultiples.push(subgrupos.pregutas);
+            }
+            for (var preguntas of subgrupos.pregutas){
+                console.log("Agrego un preguntas ", preguntas);
+                this.inputsPreguntas.push(preguntas);
+            }
+
+        }
+    }
+    
+  }
+
 
 }
