aa96ad541a1aa7d2a0ea0c70a4afa9e8cc6031d8
Alberto Robledo <roblo53@hotmail.com>
2022-09-27 23:28:19 -0300

    se resuelve error de cors en apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.web.visitar',
   appName: 'VisitAR',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    cleartext: true
+  }
 };
 
 export default config;
