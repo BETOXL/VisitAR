47da84cb5be8d697511712c9a758a5a0c3485fbf
Alberto Robledo <roblo53@hotmail.com>
2022-10-04 16:26:28 -0300

    se agrega meta info

--- android/app/src/main/java/app/web/visitar/MainActivity.java
+++ android/app/src/main/java/app/web/visitar/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.capacitorjs.plugins.preferences.PreferencesPlugin;
 import com.capacitorjs.plugins.geolocation.GeolocationPlugin;
+import com.capacitorjs.plugins.network.NetworkPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(PreferencesPlugin.class);
         registerPlugin(GeolocationPlugin.class);
+        registerPlugin(NetworkPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
